5879eeb1c14dc3d2b08ba468a51c024aab29c89e
alkizx <seyiduncan40@gmail.com>
2023-11-22 21:15:44 +0100

    anoder

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,7 @@ export default defineConfig({
       server: {
         port: 3000, // Set the development server port
       },
-      // If your project has a specific entry point, you can specify it here
-      // entry: 'path/to/your/entry.js',
     
-      // If you're using vanilla JavaScript, you may not need additional plugins,
-      // but you can configure them if necessary
-      // plugins: [yourPlugin()],
-    
-      // Configure Vite to resolve modules from the 'src' directory
       resolve: {
         alias: {
           '@': '/src',
